6eea24110c9d6ddb938a7a16fa0d9cee1e6689a0
Gayatri Ajith <gayatri@schogini.com>
2021-01-29 18:11:45 +0000

    Changed the resources to powershell

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,18 +4,31 @@
 #
 # Copyright:: 2021, The Authors, All Rights Reserved.
 
-windows_feature 'Web-Server' do 
-    action :install
+
+powershell_script 'Install IIS' do 
+  code 'Add-WindowsFeature Web-server'
+  guard_interpreter :powershell_script 
+  not_if “(Get-WindowsFeature -Name Web-Server).Installed”  
+ End 
+
+powershell_script 'Install IIS Management Console' do 
+  Code 'Add-WindowsFeature Web-Mgmt-Console' 
+  Guard_interpreter :powershell_script
+  Not_if “(Get-WindowsFeature-Name Web-MGMT-Console).Installed” 
 end 
 
-windows_feature 'Web-Mgmt-Console' do 
-   action :install 
+powershell_script 'Install ASP.NET' do 
+  code 'Add-WindowsFeature  web-ASP-Net45'
+  guard_interpreter :powershell_script
+  not_if “(Get-WindowsFeature -Name Web-Server).Installed”
+end  
+
+powershell_script 'Install IIS Static Content' do 
+  code 'Add-WindowsFeature Web-Static-Content' 
+  guard_interpreter :powershell_script
+  Not_if “(Get-WindowsFeature -Name Web-Static-Content).Installed”
 end 
 
-windows_feature 'web-ASP-Net45' do 
-   action :install
-end 
-
-windows_feature 'web-static-content' do 
-   action :install 
+service 'w3svc' do 
+  Action [:start, :enable]
 end
