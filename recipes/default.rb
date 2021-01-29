2f7b1359e6645a0c80b314a468107c00998f4f35
Gayatri Ajith <gayatri@schogini.com>
2021-01-29 18:13:23 +0000

    Fixed syntax and removed guard code

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,28 +7,20 @@
 
 powershell_script 'Install IIS' do 
   code 'Add-WindowsFeature Web-server'
-  guard_interpreter :powershell_script 
-  not_if “(Get-WindowsFeature -Name Web-Server).Installed”  
- End 
+end 
 
 powershell_script 'Install IIS Management Console' do 
-  Code 'Add-WindowsFeature Web-Mgmt-Console' 
-  Guard_interpreter :powershell_script
-  Not_if “(Get-WindowsFeature-Name Web-MGMT-Console).Installed” 
+  code 'Add-WindowsFeature Web-Mgmt-Console' 
 end 
 
 powershell_script 'Install ASP.NET' do 
   code 'Add-WindowsFeature  web-ASP-Net45'
-  guard_interpreter :powershell_script
-  not_if “(Get-WindowsFeature -Name Web-Server).Installed”
 end  
 
 powershell_script 'Install IIS Static Content' do 
   code 'Add-WindowsFeature Web-Static-Content' 
-  guard_interpreter :powershell_script
-  Not_if “(Get-WindowsFeature -Name Web-Static-Content).Installed”
 end 
 
 service 'w3svc' do 
-  Action [:start, :enable]
+  action [:start, :enable]
 end
