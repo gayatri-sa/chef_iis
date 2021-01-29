d4c90728eeddac7ea27df4288a227f5800358c99
Ubuntu <ubuntu@ip-172-31-20-251.ap-south-1.compute.internal>
2021-01-29 18:00:42 +0000

    First push

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,19 @@
 # Recipe:: default
 #
 # Copyright:: 2021, The Authors, All Rights Reserved.
+
+windows_feature 'Web-Server' do 
+    action :install
+end 
+
+windows_feature 'Web-Mgmt-Console' do 
+   action :install 
+end 
+
+windows_feature 'web-ASP-Net45' do 
+   action :install
+end 
+
+windows_feature 'web-static-content' do 
+   action :install 
+end
