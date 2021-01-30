7847d3013e879c1427e5051b501fb8caae793d71
Gayatri Ajith <gayatri@schogini.com>
2021-01-30 02:47:43 +0000

    Fixed the resources and added conditional execution of the powershell commands

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,18 +7,22 @@
 
 powershell_script 'Install IIS' do 
   code 'Add-WindowsFeature Web-server'
+  not_if 'iwr http://localhost/ -UseBasicParsing'
 end 
 
 powershell_script 'Install IIS Management Console' do 
   code 'Add-WindowsFeature Web-Mgmt-Console' 
+  not_if 'iwr http://localhost/ -UseBasicParsing'
 end 
 
 powershell_script 'Install ASP.NET' do 
   code 'Add-WindowsFeature  web-ASP-Net45'
+  not_if 'iwr http://localhost/ -UseBasicParsing'
 end  
 
 powershell_script 'Install IIS Static Content' do 
   code 'Add-WindowsFeature Web-Static-Content' 
+  not_if 'iwr http://localhost/ -UseBasicParsing'
 end 
 
 service 'w3svc' do 
