807068efbcf2b784be264949906fc40a9c9659e0
Gayatri Ajith <gayatri@schogini.com>
2021-01-31 11:33:14 +0000

    Changed the version number

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
 name 'iis'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'Gayatri S Ajith'
+maintainer_email 'gayatri@schogini.com'
 license 'All Rights Reserved'
 description 'Installs/Configures iis'
-version '0.1.0'
+version '0.2.0'
 chef_version '>= 15.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
